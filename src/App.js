dbec2ab5e61cc31fdf40c74e50cfabb29f0d677f
Tavo <o.enriquez88@gmail.com>
2022-05-04 21:34:53 -0700

    so far only renders one pokemon on page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      allPokemon: [],
+      searchField: "",
+    };
+  }
+
+  componentDidMount() {
+    const fetchPokemon = () => {
+      fetch("https://pokeapi.co/api/v2/pokemon?limit=5&offset=0")
+        .then((response) => response.json())
+        .then((allPokemon) => {
+          allPokemon.results.forEach((pokemon) => {
+            fetchPokemonData(pokemon);
+          });
+        });
+    };
+
+    const fetchPokemonData = (pokemon) => {
+      let url = pokemon.url;
+      console.log(url);
+      fetch(url)
+        .then((response) => response.json())
+        .then((pokeData) =>
+          this.setState(
+            () => {
+              return { allPokemon: [pokeData] };
+            },
+            () => {
+              console.log(this.state);
+            }
+          )
+        );
+    };
+
+    fetchPokemon();
+  }
+
+  render() {
+    const { allPokemon, searchField } = this.state;
+
+    const filteredPokemon = allPokemon.filter((pokemon) => {
+      return pokemon.name.toLowerCase().includes(searchField);
+    });
+
+    return (
+      <div className="App">
+        <input
+          className="search-box"
+          type="search"
+          placeholder="search pokemon"
+          onChange={(event) => {
+            console.log(event.target.value);
+            const searchField = event.target.value.toLowerCase();
+
+            this.setState(() => {
+              return { searchField };
+            });
+          }}
+        />
+        ;
+        {filteredPokemon.map((pokeData) => {
+          return (
+            <div key={pokeData.id}>
+              <h1>{pokeData.name}</h1>
+              <img src={pokeData.sprites.front_default} alt="pokemon" />
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
